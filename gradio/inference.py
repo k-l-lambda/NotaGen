19c2226473e26cd2d17dc4c8a18be6b96feab8ec
Ezra Epstein <3422531+eepstein@users.noreply.github.com>
2025-03-26 14:14:01 -0700

    Update inference.py
    Add support for Apple Silicon "mps" device_type.

--- gradio/inference.py
+++ gradio/inference.py
@@ -13,6 +13,8 @@ Note_list = Note_list + ['z', 'x']
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
 else:
     device = torch.device("cpu")
 
