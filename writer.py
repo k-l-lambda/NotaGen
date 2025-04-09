44b8ff385f173700606ed18656a3d80361481945
K.L. Λ <k.l.lambda@gmail.com>
2025-04-09 20:30:43 +0800

    added safe quit by sigint.

--- writer.py
+++ writer.py
@@ -3,6 +3,7 @@ import os
 import typer
 import random
 import hashlib
+import signal
 
 from gradio_app.inference import postprocess_inst_names
 from gradio_app.inference import inference_patch
@@ -23,6 +24,15 @@ def main(
 	prompt_list = [line.strip().split('_') for line in prompt_list]
 	assert all(len(args) == 3 for args in prompt_list)
 
+	global to_quit
+	to_quit = False
+
+	def handle_sigint(sig, _):
+		print(f"Signal {sig} received. Requesting safe shutdown...")
+		global to_quit
+		to_quit = True
+	signal.signal(signal.SIGINT, handle_sigint)
+
 	for i in range(n):
 		period, composer, instrumentation = random.choice(prompt_list)
 		print(f"\033[1;94mGenerating {i+1}/{n} piece...\033[0m")
@@ -30,6 +40,10 @@ def main(
 		md5_hash = hashlib.md5(abc_content.encode('utf-8')).hexdigest()
 		with open(f'{target_dir}/{md5_hash}.abc', 'w') as f:
 			f.write(abc_content)
+
+		if to_quit:
+			print("Safe shutdown.")
+			break
 
 if __name__ == "__main__":
 	app()
